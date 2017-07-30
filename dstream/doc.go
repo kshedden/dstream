70696cd234c2971a6a2bbefc1bc0fd21be176f4c
Kerby Shedden <kshedden@umich.edu>
2017-07-30 15:05:36 -0400

    update package documentation

--- dstream/doc.go
+++ dstream/doc.go
@@ -2,18 +2,19 @@
 // typed, multivariate data.  A Dstream is a data container that
 // (conceptually) holds a rectangular array of data in which the
 // columns are variables and the rows are cases or observations.  The
-// dstream framework facilitates processing data of this type, with a
-// focus on feeding the data into statistical modeling tools.
+// dstream framework facilitates processing data of this type in a
+// streaming manner, with a focus on feeding the data into statistical
+// modeling tools.
 //
-// The data held by a Dstream is stored column-wise, and each column
-// is partitioned into chunks.  A Dstream visits its chunks in order,
-// and in general, a single chunk resides in memory at one point in
-// time.  When processing a Dstream, call Next to advance to the next
-// chunk, then call Get to retrieve the data for one column.
+// The data held by a Dstream is stored as chunks of contiguous rows,
+// and within each chunk the data are stored column-wise.  A Dstream
+// visits its chunks in order.  When processing a Dstream, call Next
+// to advance to the next chunk, then call Get to retrieve the data
+// for one column.
 //
 // Most operations on Dstreams take the form of a transformation d =
 // f(d), where d is a Dstream.  Many transformations are defined in
 // the package, and it is easy to add new transformations.  Examples
-// of transformations are Mutate (modify a column in-place) and
-// DropCols (drop a set of columns from the Dstream).
+// of transformations are Mutate (modify a column in-place) and Drop
+// (drop one or more columns from the Dstream).
 package dstream
