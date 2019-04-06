6c9e951b258fbf6920dcd2977ab86b67c36a62d5
Kerby Shedden <kshedden@umich.edu>
2019-04-06 13:16:18 -0400

    update

--- dstream/linapply_test.go
+++ dstream/linapply_test.go
@@ -4,40 +4,42 @@ import "testing"
 
 func TestLinapply1(t *testing.T) {
 
-	x1 := []interface{}{
-		[]float64{0, 0, 0},
-		[]float64{1, 1, 1},
-		[]float64{2, 2, 3},
+	x := [][]interface{}{
+		{
+			[]float64{0, 0, 0},
+			[]float64{1, 1, 1},
+			[]float64{2, 2, 3},
+		},
+		{
+			[]string{"a", "b", "c"},
+			[]string{"d", "e", "f"},
+			[]string{"g", "h", "i"},
+		},
+		{
+			[]float64{1, 2, 3},
+			[]float64{4, 5, 6},
+			[]float64{7, 8, 9},
+		},
 	}
-	x2 := []interface{}{
-		[]string{"a", "b", "c"},
-		[]string{"d", "e", "f"},
-		[]string{"g", "h", "i"},
-	}
-	x3 := []interface{}{
-		[]float64{1, 2, 3},
-		[]float64{4, 5, 6},
-		[]float64{7, 8, 9},
-	}
-	dat := [][]interface{}{x1, x2, x3}
 	na := []string{"x1", "x2", "x3"}
-	da := NewFromArrays(dat, na)
+	da := NewFromArrays(x, na)
 
-	z1 := []interface{}{
-		[]float64{2, 4, 6},
-		[]float64{9, 11, 13},
-		[]float64{16, 18, 21},
-	}
+	x = append(x,
+		[][]interface{}{
+			{
+				[]float64{2, 4, 6},
+				[]float64{9, 11, 13},
+				[]float64{16, 18, 21},
+			},
+			{
+				[]float64{1, 2, 3},
+				[]float64{3, 4, 5},
+				[]float64{5, 6, 6},
+			},
+		}...)
 
-	z2 := []interface{}{
-		[]float64{1, 2, 3},
-		[]float64{3, 4, 5},
-		[]float64{5, 6, 6},
-	}
-
-	dat = [][]interface{}{x1, x2, x3, z1, z2}
 	na = []string{"x1", "x2", "x3", "z0", "z1"}
-	ex := NewFromArrays(dat, na)
+	ex := NewFromArrays(x, na)
 
 	coeffs := [][]float64{
 		[]float64{1, 0, 2},
